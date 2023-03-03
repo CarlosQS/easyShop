69ee07c068c6b6cf9e8f6b968e009d2934efe961
Carlos <carlos.qs.01.cqs@gmail.com>
2023-03-03 12:53:39 +0100

    Edited phpdoc routes

--- routes/api.php
+++ routes/api.php
@@ -8,15 +8,9 @@ use Illuminate\Support\Facades\Route;
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| Here is where I registered API routes for my application.
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::get('/tiendas', function () {
     return \App\Models\Tienda::all()->load('productos')->toJson(JSON_PRETTY_PRINT);
